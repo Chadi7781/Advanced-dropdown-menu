e2f038bf02bbe36c5e4f2c4b1b34f5f897a5f65e
Chadi Troudi <troudishedy6@gmail.com>
2021-11-24 21:22:42 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <Navbar></Navbar>
+
+  );
+}
+
+
+function Navbar() {
+  return(
+    <nav className="navbar">
+      <ul className="navbar-nav">
+
+      </ul>
+
+    </nav>
+
+  )
+}
+export default App;
